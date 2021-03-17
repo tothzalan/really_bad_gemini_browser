914a99f914c5a93c2b10ce53f9bfc0978f5e866a
tothzalan <toth.zalan03@gmail.com>
2021-03-17 20:41:57 +0100

    Updated Parser Solved ArgumentOutOfRangeException when using Substrings

--- WpfApp/WpfApp/Parser.cs
+++ WpfApp/WpfApp/Parser.cs
@@ -15,39 +15,43 @@ namespace WpfApp
             {
                 return new LineModel(TypeOfLine.EMPTY, "");
             }
-            if (line.Substring(0, 3) == "###") //h3
+            if (line.Trim().Length > 1)
             {
-                return new LineModel(TypeOfLine.H3, line.Substring(3).Trim());
+                if (line.Substring(0, 1) == "#" && line[1] != '#') //h1
+                {
+                    return new LineModel(TypeOfLine.H1, line.Substring(1).Trim());
+                }
+                else if (line.Substring(0, 1) == ">") //quote
+                {
+                    return new LineModel(TypeOfLine.QUOTE, line.Substring(1).Trim());
+                }
+                else if (line.Substring(0, 1) == "*") //list
+                {
+                    return new LineModel(TypeOfLine.LIST, line.Substring(1).Trim());
+                }
             }
-            else if (line.Substring(0, 2) == "##") //h2
+            if (line.Trim().Length > 2) {
+                if (line.Substring(0, 2) == "##" && line[2] != '#') //h2
+                {
+                    return new LineModel(TypeOfLine.H2, line.Substring(2).Trim());
+                }
+                else if (line.Substring(0, 2) == "=>") //link
+                {
+                    return new LineModel(TypeOfLine.LINK, line.Substring(2).Trim());
+                }
+            }
+            if (line.Trim().Length > 3)
             {
-                return new LineModel(TypeOfLine.H2, line.Substring(2).Trim());
+                if (line.Substring(0, 3) == "###") //h3
+                {
+                    return new LineModel(TypeOfLine.H3, line.Substring(3).Trim());
+                }
+                else if (line.Substring(0, 3) == "```") //pre
+                {
+                    return new LineModel(TypeOfLine.PRE, line.Substring(3).Trim());
+                }
             }
-            else if (line.Substring(0, 1) == "#") //h1
-            {
-                return new LineModel(TypeOfLine.H1, line.Substring(1).Trim());
-            }
-            else if (line.Substring(0, 2) == "=>") //link
-            {
-                return new LineModel(TypeOfLine.LINK, line.Substring(2).Trim());
-            }
-            else if (line.Substring(0, 1) == ">") //quote
-            {
-                return new LineModel(TypeOfLine.QUOTE, line.Substring(1).Trim());
-            }
-            else if (line.Substring(0, 1) == "*") //list
-            {
-                return new LineModel(TypeOfLine.LIST, line.Substring(1).Trim());
-            }
-            else if (line.Substring(0, 3) == "```") //pre
-            {
-                return new LineModel(TypeOfLine.PRE, line.Substring(3).Trim());
-
-            }
-            else //text
-            {
-                return new LineModel(TypeOfLine.LINE, line.Trim());
-            }
+            return new LineModel(TypeOfLine.LINE, line);
         }
         public List<LineModel> FromFile(string fileName)
         {
@@ -57,6 +61,7 @@ namespace WpfApp
             {
                 string line = sr.ReadLine();
                 lines.Add(ParseLine(line));
+                Console.WriteLine(ParseLine(line));
             }
             return lines;
         }
